3752163e6c4796ac986c6c3fe7607a437e598549
GMartin03 <martingomez2003174@hotmail.com>
2025-03-30 00:38:03 -0300

    punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,14 @@
 
 int main () {
 
-    printf("Hola Mundo");
+    int var = 42; 
+    int *puntero = &var;  
+
+    printf("El contenido del puntero: %d\n", *puntero);
+    printf("La dirección de memoria almacenada por el puntero: %p\n", puntero);
+    printf("La dirección de memoria de la variable: %p\n", &var);
+    printf("La dirección de memoria del puntero: %p\n", &puntero);
+    printf("Tamaño de memoria utilizado por la variable: %zu bytes\n", sizeof(var));
+
     return 0;
 }
